294e1e0f5ce155e7ab5dcac0b46ad355c1028c5a
alx <world.zay@gmail.com>
2025-06-06 11:26:07 +0300

    Update ApiValidationMiddleware

--- src/Middleware/ApiValidationMiddleware.php
+++ src/Middleware/ApiValidationMiddleware.php
@@ -1,10 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Az\Validation\Middleware;
 
 use Az\Validation\Validation;
 use HttpSoft\Response\JsonResponse;
-use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -12,38 +13,63 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 abstract class ApiValidationMiddleware implements MiddlewareInterface
 {
-    protected Validation $validation;
-    protected ContainerInterface $container;
-
-    public function __construct(Validation $validation, ContainerInterface $container)
-    {
-        $this->validation = $validation;
-        $this->container = $container;
-    }
+    public function __construct(protected Validation $validation) {}
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $this->setRules();
+        $lang = $request->getHeaderLine('Accept-Language') ?? 'en';
+        $this->validation->setLang($lang);
+
+        $this->setRules($request);
 
         if (($response = $this->validate($request, $handler))) {
             return $response;
         }
 
-        return new JsonResponse($this->validation->getResponse());
+        $validation_response = $this->validation->getResponse();
+
+        $validation_response = array_map(function ($a) {
+            return [
+                'status' => $a['status'],
+                'msg' => $a['msg'],
+            ];
+        }, $validation_response);
+
+        $validation_response['success'] = false;
+        return new JsonResponse($validation_response);
     }
 
-    protected function setRules() {}
+    protected function setRules(ServerRequestInterface $request) {}
+
+    protected function modifyData($data)
+    {
+        return $data;
+    }
 
     protected function validate(ServerRequestInterface $request, RequestHandlerInterface $handler): ?ResponseInterface
     {
-        $data = $request->getBody()->getContents();
-        $data = json_decode($data, true)??[];
+        $data = $this->getData($request);
         $files = $request->getUploadedFiles();
 
         if ($this->validation->check($data, $files)) {
-            return $handler->handle($request->withAttribute('post', $data));
+            return $handler->handle($request->withAttribute('data', $data));
         }
 
         return null;
+    }
+
+    private function getData($request)
+    {
+        $data = $request->getBody()->getContents();
+
+        if (empty($data)) {
+            $data = $request->getParsedBody();
+        }
+
+        if (is_string($data)) {
+            $data = json_decode($data, true) ?? [];
+        }
+
+        return $data;
     }
 }
