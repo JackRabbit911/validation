70cdd88048f7f8a475d5a69a7e879e6496afc830
alx <world.zay@gmail.com>
2025-11-16 22:41:51 +0300

    #15-hotfix-getresponse Remove is_array($value) handle

--- src/ValidationValue.php
+++ src/ValidationValue.php
@@ -12,8 +12,7 @@ final class ValidationValue
     public function __construct(
         private Response $response,
         private array $rules,
-        private array $data,
-        private ?string $userHandler,
+        private ?string $userHandler = null,
     ) {
         $this->parser = new Parser();
         $this->resolver = new Resolver($userHandler);
@@ -22,38 +21,19 @@ final class ValidationValue
     public function check($value, $key)
     {
         $rule = $this->rules[$key];
-        $validate = function ($value, $rule, $is_array = false) use ($key) {
-            $field_rules = $this->parser->parse($rule);
+        $field_rules = $this->parser->parse($rule);
 
-            foreach ($field_rules as $rule) {
+        foreach ($field_rules as $rule) {
+            $params = $this->santizeParams($rule->params, $value);
+            $handler = $this->resolver->resolve($rule->handler);
+            $result = call_user_func_array($handler, $params);
+            $result = ($rule->inverse) ? !$result : $result;
 
-                $params = $this->santizeParams($rule->params, $value);
-                $handler = $this->resolver->resolve($rule->handler);
-                $result = call_user_func_array($handler, $params);
-                $result = ($rule->inverse) ? !$result : $result;
-
-                if ($result !== true) {
-                    array_shift($params);
-                    $params[] = $is_array ? [] : '';
-                    $this->response->setErrorData($key, $rule->handler, $params);
-                    break;
-                }
+            if ($result !== true) {
+                array_shift($params);
+                $this->response->setErrorData($key, $rule->handler, $params);
+                break;
             }
-
-            return $result;
-        };
-
-        if (is_array($value)) {
-            $result = true;
-            foreach ($value as $val) {
-                $res = $validate($val, $rule, true);
-                if ($res !== true) {
-                    $result = $res;
-                    break;
-                }
-            }
-        } else {
-            $result = $validate($value, $rule);
         }
 
         return $result;
