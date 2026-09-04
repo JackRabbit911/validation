31afcd155e0681847ab57bdbfa980e552007493e
alx <world.zay@gmail.com>
2026-09-04 19:29:42 +0300

    #36 Add some functions ans update construct

--- src/Validation.php
+++ src/Validation.php
@@ -8,20 +8,10 @@ class Validation
     private array $rules = [];
     private array $check = [];
     private ?string $userHandler = null;
-    private int $status_code = 200;
 
-    public function __construct(private ValidationResponseInterface $response, ?array $options = null)
-    {
-        if ($options) {
-            foreach ($options as $key => $value) {
-                if (property_exists($this, $key)) {
-                    $this->$key = $value;
-                }
-            }
-        }
-    }
+    public function __construct(private Response $response){}
 
-    public function rule($name, $handler, ...$params): self
+    public function rule($name, $handler, ...$params)
     {
         $value = [
             'handler' => $handler,
@@ -37,13 +27,11 @@ class Validation
         return $this;
     }
 
-    public function reset(): self
+    public function reset()
     {
         $this->rules = [];
         $this->data = [];
         $this->check = [];
-
-        return $this;
     }
 
     public function getRules(): array
@@ -77,40 +65,42 @@ class Validation
         return in_array(false, $this->check) ? false : true;
     }
 
-    public function getResponse($is_api = false): array
+    public function getResponse($is_api = false)
     {
         return $is_api ?  $this->response->getApiResponse($this->data) :
             $this->response->getResponse($this->data, array_keys($this->rules));
     }
 
-    public function response(): ValidationResponseInterface
+    public function getMessage($key)
     {
-        return $this->response;
+        return $this->response->getMessage($key);
     }
 
-    public function statusCode(?int $status_code = null): self | int
-    {
-        if ($status_code) {
-            $this->status_code = $status_code;
-            return $this;
-        }
-
-        return $this->status_code;
-    }
-
-    public function setUserHandler(string $className): self
+    public function setUserHandler(string $className)
     {
         $this->userHandler = $className;
         return $this;
     }
 
-    public function addMsgPath($path): self
+    public function setMsgKey($name, $key)
+    {
+        $this->response->setMsgKey($name, $key);
+        return $this;
+    }
+
+    public function addMsgPath($path)
     {
         $this->response->addMsgPath($path);
         return $this;
     }
 
-    public function setLang($lang): self
+    public function setMessage($name, $msg)
+    {
+        $this->response->setMessage($name, $msg);
+        return $this;
+    }
+
+    public function setLang($lang)
     {
         $this->response->setLang($lang);
         return $this;
